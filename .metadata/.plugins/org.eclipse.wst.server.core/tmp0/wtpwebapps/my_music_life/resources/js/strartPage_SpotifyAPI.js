4dbbebf072068a731ea0f8def297ac58233c7532
budflower44 <budflower44@gmail.com>
2024-02-16 15:40:53 +0900

    mml project first commit - albumsSearchData DB Connenct, front layout

---
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/my_music_life/resources/js/strartPage_SpotifyAPI.js
@@ -0,0 +1,108 @@
+async function getTokenToServer(){
+    try {
+        const url = "/mml/token";
+        const config = {
+            method:'post'
+        };
+        const resp = await fetch(url, config);
+        const result = await resp.text();
+        // console.log("getTokenToServer result >> "+result);
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function fetchWebApi(token, keyword) {
+    var res1 = encodeURI(keyword);
+    console.log(keyword);
+    const res = await fetch(`https://api.spotify.com/v1/search?q=${keyword}&type=album%2Ctrack%2Cartist`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        method : 'GET'
+    });
+  const data = await res.json();
+  console.log(data);
+  return data;
+}
+
+document.getElementById('searchBtn').addEventListener('click', async ()=>{
+    try {
+        const keywordVal = document.getElementById('searchKeyword').value;
+        console.log(keywordVal);
+        getTokenToServer().then(token=>{
+            fetchWebApi(token, keywordVal).then(res =>{
+                if(res.albums){
+                    searchAlbumsDataToServer(res.albums);
+                    for(let i=0; i<res.albums.items.length; i++){
+                        console.log(res.albums.items[i].external_urls.spotify);
+                        const url = res.albums.items[i].external_urls.spotify;
+                        const embedUrl = url.match(/\/album\/(\w+)/)[0];
+                        console.log(embedUrl);
+                    }
+                }
+                if(res.artists){
+                    for(let i=0; i<res.artists.items.length; i++){
+                        console.log(res.artists.items[i].external_urls.spotify);
+                        const url = res.artists.items[i].external_urls.spotify;
+                        const embedUrl = url.match(/\/artist\/(\w+)/)[0];
+                        console.log(embedUrl);
+                    }
+                }
+                if(res.tracks){
+                    for(let i=0; i<res.tracks.items.length; i++){
+                        console.log(res.tracks.items[i].external_urls.spotify);
+                        const url = res.tracks.items[i].external_urls.spotify;
+                        const embedUrl = url.match(/\/track\/(\w+)/)[0];
+                        console.log(embedUrl);
+                    }
+                }
+                
+                location.href="/mml/main?"+keywordVal;
+                
+            })
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+async function searchAlbumsDataToServer(searchData){
+    try {
+        const url = "/mml/search/albums";
+        const config = {
+            method : 'post',
+            headers : {
+                'content-type' : 'application/json; charset=utf-8'
+            },
+            body : JSON.stringify(searchData)
+        };
+        const resp = await fetch(url, config);
+        const result = await resp.text();
+        console.log("서버 응답 받음:", result);
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function searchArtistsDataToServer(searchData){
+    try {
+        const url = "/mml/search/artist"
+        const config = {
+            method: 'post',
+            headers : {
+                'content-type' : 'appication/json; charset=utf-8'
+            },
+            body : JSON.stringify(searchData)
+        };
+        const resp = await fetch(url, config);
+        const result = await resp.text();
+        console.log("서버 응답 받은", result);
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
---
+++ my_music_life/src/main/java/com/mml/www/service/SpotifyAPIService.java
@@ -0,0 +1,10 @@
+package com.mml.www.service;
+
+import com.mml.www.domain.AlbumsVO;
+
+public interface SpotifyAPIService {
+
+	void insertDBAlbums(AlbumsVO albumsVO);
+
+	
+}
